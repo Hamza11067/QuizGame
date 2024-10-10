69a67861dfe615baecea64f2caffbddb90bcbc6f
Hamza11067 <hamzakhalid1067@gmail.com>
2024-10-10 12:57:28 +0500

    making quiz game using classes in js

---
+++ quiz.js
@@ -0,0 +1,35 @@
+class Question {
+  constructor(quizContainerId) {
+    this.quizContainer = document.getElementById(quizContainerId);
+    this.fetchQuestions();
+    this.fetchOptions();
+    this.fetchCorrectOptions();
+  }
+  fetchQuestions() {
+    let questions = this.quizContainer.getElementsByClassName("quiz-section");
+
+    this.questions = Array.from(questions).map((question) => {
+      return question.getElementsByClassName("question")[0].innerHTML;
+    });
+
+    console.log(this.questions);
+  }
+  fetchOptions() {
+    let options = this.quizContainer.getElementsByClassName("quiz-options");
+    this.options = Array.from(options).map((option) =>
+      Array.from(option.getElementsByTagName("li")).map((opt) => opt.innerHTML)
+    );
+    console.log(this.options);
+  }
+
+  fetchCorrectOptions() {
+    let correctOptions =
+      this.quizContainer.getElementsByClassName("quiz-options");
+    this.correctOptions = Array.from(correctOptions).map((correctOption) => {
+      return correctOption.getElementsByClassName("correct")[0].innerHTML;
+    });
+    console.log(this.correctOptions);
+  }
+}
+
+let question = new Question("quizContainer");
