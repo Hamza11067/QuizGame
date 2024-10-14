f3eed8ef2b0e5deb676bd3837db4d5d946b120d7
Hamza11067 <hamzakhalid1067@gmail.com>
2024-10-14 10:38:22 +0500

    added method for rendering questions

--- quiz.js
+++ quiz.js
@@ -1,20 +1,24 @@
 class Question {
   constructor(quizContainerId) {
     this.quizContainer = document.getElementById(quizContainerId);
+    this.currentQuestionIndex = 0;
     this.fetchQuestions();
     this.fetchOptions();
     this.fetchCorrectOptions();
-    this.isCorrect();
+    this.renderQuestion();
+    // this.isCorrect();
   }
+
   fetchQuestions() {
     let questions = this.quizContainer.getElementsByClassName("quiz-section");
 
     this.questions = Array.from(questions).map((question) => {
-      return question.getElementsByClassName("question")[0].innerHTML;
+      return question.getElementsByClassName("question");
     });
 
     console.log(this.questions);
   }
+
   fetchOptions() {
     let options = this.quizContainer.getElementsByClassName("quiz-options");
     this.options = Array.from(options).map((option) =>
@@ -30,6 +34,24 @@ class Question {
       return correctOption.getElementsByClassName("correct")[0].innerHTML;
     });
     console.log(this.correctOptions);
+  }
+
+  renderQuestion() {
+    let quizBlocks = this.quizContainer.getElementsByClassName("quiz-section");
+
+    Array.from(quizBlocks).forEach((questionBlock, index) => {
+      if (index == this.currentQuestionIndex) {
+        questionBlock.style.display = "block";
+      } else {
+        questionBlock.style.display = "none";
+      }
+    });
+
+    // this.questions.forEach((question, index) => {
+    //   if (index === this.currentQuestionIndex) {
+    //     question.style.display = "block";
+    //   }
+    // });
   }
 }
 
